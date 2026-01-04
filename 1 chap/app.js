042349ae966fbea185174388f5bd2443e2806abd
Syeda Nabiha <syedanabiha822@gmail.com>
2026-01-04 17:15:24 +0500

    1 java chap

---
+++ 1 chap/app.js
@@ -0,0 +1,16 @@
+// 1. Greet website visitor
+alert("Welcome to my website!");
+
+// 2. Display error message
+alert("Error! Please enter a valid username.");
+
+// 3. Line break message
+alert("Welcome to JS Land...\nHappy Coding!");
+
+// 4. Messages in sequence
+alert("Welcome to JS world...");
+alert("Happy Coding!\nPrevent this page from creating additional dialogs.");
+
+// 5. Message in browser console
+console.log("Hello... I can run JS through my web browser's console");
+
