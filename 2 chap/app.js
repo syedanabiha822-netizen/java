4257466092b087a2cdd73f8320923ac0ee6dd00f
Syeda Nabiha <syedanabiha822@gmail.com>
2026-01-04 17:16:42 +0500

    2 java chap

---
+++ 2 chap/app.js
@@ -0,0 +1,40 @@
+var username;
+
+// 2. Full name
+var myName = "Syeda Nabiha";
+
+// 3. Hello World message
+var message = "Hello this is my web";
+alert(message);
+
+// 4. Student bio data
+var studentName = "Syeda Nabiha"
+var studentNic = "Biya";
+var studentAge = "14 years old";
+var studentCourse = "JavaScript";
+
+alert(studentName);
+alert(studentNic);
+alert(studentAge);
+alert(studentCourse);
+
+// 5. Pizza pattern
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+
+var email = "syedanabiha822@gmail.com";
+alert("My email address is " + email);
+
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+
+
+
+document.write("Yah! I can write HTML content through JavaScript<br><br>");
+
+var design = "*▬▬▬▬▬▬▬▬▬ஜ▬▬▬▬▬▬▬▬▬*";
+alert(design);
+document.write(design);
+
+
