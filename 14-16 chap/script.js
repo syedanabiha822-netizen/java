58d9d143cd62710787e7acf488d8499da3cf97d7
Syeda Nabiha <syedanabiha822@gmail.com>
2026-01-08 02:08:00 +0500

    17-20 java chap

--- 14-16 chap/script.js
+++ 14-16 chap/script.js
@@ -1,32 +1,32 @@
 /* Q1 */
 var studentsLiteral = [];
 document.getElementById("q1").innerHTML =
-"<h2>Q1</h2>Empty Array (Literal): " + studentsLiteral;
+  "<h2>Q1</h2>Empty Array (Literal): " + studentsLiteral;
 
 /* Q2 */
 var studentsObject = new Array();
 document.getElementById("q2").innerHTML =
-"<h2>Q2</h2>Empty Array (Object): " + studentsObject;
+  "<h2>Q2</h2>Empty Array (Object): " + studentsObject;
 
 /* Q3 */
 var stringArray = ["Apple", "Banana", "Mango"];
 document.getElementById("q3").innerHTML =
-"<h2>Q3</h2>String Array: " + stringArray;
+  "<h2>Q3</h2>String Array: " + stringArray;
 
 /* Q4 */
 var numberArray = [10, 20, 30, 40];
 document.getElementById("q4").innerHTML =
-"<h2>Q4</h2>Number Array: " + numberArray;
+  "<h2>Q4</h2>Number Array: " + numberArray;
 
 /* Q5 */
 var booleanArray = [true, false, true];
 document.getElementById("q5").innerHTML =
-"<h2>Q5</h2>Boolean Array: " + booleanArray;
+  "<h2>Q5</h2>Boolean Array: " + booleanArray;
 
 /* Q6 */
 var mixedArray = ["Biya", 14, true];
 document.getElementById("q6").innerHTML =
-"<h2>Q6</h2>Mixed Array: " + mixedArray;
+  "<h2>Q6</h2>Mixed Array: " + mixedArray;
 
 /* Q7 */
 var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil", "PhD"];
@@ -35,7 +35,7 @@ for (var i = 0; i < qualifications.length; i++) {
   edu += (i + 1) + ") " + qualifications[i] + "<br>";
 }
 document.getElementById("q7").innerHTML =
-"<h2>Q7</h2>Education Qualifications:<br>" + edu;
+  "<h2>Q7</h2>Education Qualifications:<br>" + edu;
 
 /* Q8 */
 var names = ["Ali", "Ahmed", "Sara"];
@@ -46,10 +46,10 @@ var result = "";
 for (var i = 0; i < names.length; i++) {
   var per = (scores[i] / totalMarks) * 100;
   result += names[i] + " scored " + scores[i] +
-   " marks (" + per.toFixed(2) + "%)<br>";
+    " marks (" + per.toFixed(2) + "%)<br>";
 }
 document.getElementById("q8").innerHTML =
-"<h2>Q8</h2>" + result;
+  "<h2>Q8</h2>" + result;
 /* Q9 */
 var colors = ["Red", "Green", "Blue"];
 
@@ -74,36 +74,35 @@ var delCount = prompt("How many colors to delete:");
 colors.splice(delIndex, delCount);
 
 document.getElementById("q9").innerHTML =
-"<h2>Q9</h2>Final Colors Array:<br>" + colors;
+  "<h2>Q9</h2>Final Colors Array:<br>" + colors;
 
 /* Q10 */
 var studentScores = [320, 230, 480, 120];
-studentScores.sort(function(a, b) {
+studentScores.sort(function (a, b) {
   return a - b;
 });
 document.getElementById("q10").innerHTML =
-"<h2>Q10</h2>Sorted Scores (Ascending): " + studentScores;
+  "<h2>Q10</h2>Sorted Scores (Ascending): " + studentScores;
 
 /* Q11 */
 var cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
 var selectedCities = cities.slice(1, 4);
 document.getElementById("q11").innerHTML =
-"<h2>Q11</h2>Cities: " + cities + "<br>Selected Cities: " + selectedCities;
+  "<h2>Q11</h2>Cities: " + cities + "<br>Selected Cities: " + selectedCities;
 
 /* Q12 */
 var arr = ["This", "is", "my", "cat"];
 var sentence = arr.join(" ");
 document.getElementById("q12").innerHTML =
-"<h2>Q12</h2>Joined String: " + sentence;
+  "<h2>Q12</h2>Joined String: " + sentence;
 
 /* Q13 */
 var fifo = [];
 fifo.push("Keyboard");
 fifo.push("Mouse");
 fifo.push("Printer");
-
 var removedItem = fifo.shift();
-
 document.getElementById("q13").innerHTML =
-"<h2>Q13</h2>FIFO Example:<br>Removed: " + removedItem +
-"<br>Remaining: " + fifo;
+  "<h2>Q13</h2>FIFO Example:<br>Removed: " + removedItem +
+  "<br>Remaining: " + fifo;
+  
---
+++ 17-20 chap/script.js
@@ -0,0 +1,109 @@
+let output = document.getElementById("output");
+
+// Q1
+function q1() {
+  let arr = [[], [], []];
+  output.innerHTML = "Empty Multidimensional Array:<br>" + JSON.stringify(arr);
+}
+
+// Q2
+function q2() {
+  let matrix = [
+    [0, 1, 2, 3],
+    [1, 0, 1, 2],
+    [2, 1, 0, 1]
+  ];
+  output.innerHTML = matrix.join("<br>");
+}
+
+// Q3
+function q3() {
+  let res = "";
+  for (let i = 1; i <= 10; i++) {
+    res += i + " ";
+  }
+  output.innerHTML = res;
+}
+
+// Q4
+function q4() {
+  let num = +prompt("Enter table number");
+  let len = +prompt("Enter length");
+  let res = "";
+  for (let i = 1; i <= len; i++) {
+    res += `${num} x ${i} = ${num * i}<br>`;
+  }
+  output.innerHTML = res;
+}
+
+// Q5
+function q5() {
+  let fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+  let res = "";
+  for (let i = 0; i < fruits.length; i++) {
+    res += fruits[i] + "<br>";
+  }
+  output.innerHTML = res;
+}
+
+// Q6
+function q6() {
+  let res = "<b>Counting:</b><br>";
+  for (let i = 1; i <= 15; i++) res += i + ", ";
+
+  res += "<br><b>Reverse:</b><br>";
+  for (let i = 10; i >= 1; i--) res += i + ", ";
+
+
+  res += "<br<b>reverse</b><br>"
+
+
+  res += "<br><b>Even:</b><br>";
+  for (let i = 0; i <= 20; i += 2) res += i + ", ";
+
+  res += "<br><b>Odd:</b><br>";
+  for (let i = 1; i <= 19; i += 2) res += i + ", ";
+
+  res += "<br><b>Series:</b><br>";
+  for (let i = 2; i <= 20; i += 2) res += i + "k, ";
+
+  output.innerHTML = res;
+}
+
+// Q7
+function q7() {
+  let items = ["cake", "apple pie", "cookie", "chips", "patties"];
+  let search = prompt("Enter item").toLowerCase();
+
+  if (items.includes(search)) {
+    output.innerHTML = search + " is available";
+  } else {
+    output.innerHTML = search + " is not available";
+  }
+}
+
+
+// Q8
+function q8() {
+  let A = [24, 53, 78, 91, 12];
+  let max = Math.max(...A);
+  output.innerHTML = "Largest Number: " + max;
+}
+
+
+
+// Q9
+function q9() {
+  let A = [24, 53, 78, 91, 12];
+  let min = Math.min(...A);
+  output.innerHTML = "Smallest Number: " + min;
+}
+
+// Q10
+function q10() {
+  let res = "";
+  for (let i = 5; i <= 100; i += 5) {
+    res += i + ", ";
+  }
+  output.innerHTML = res;
+}
